c1bbc3bd73bbebc381d4de730eabc14ab12ab1d1
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2022-07-17 14:04:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,31 @@ def random_with_N_digits(n):
     range_end = (10**n)-1
     return randint(range_start, range_end)
 
-rowCount = 0;
+def generate(inputCount):
+    rowCount = 0;
 
-while(True):
-    rowCount += 1;
-    randomNum = random_with_N_digits(7)
+    # while(True):
+    while(rowCount < int(inputCount)):
+        rowCount += 1;
+        randomNum = random_with_N_digits(7)
+        print (rowCount,"#", concat(0, randomNum))
     
-    regigsterUrl = 'https://fuelpass.gov.lk/api/otp/registration'
-    registeredObject = {'mobileNo': concat(0, randomNum)}
-    x = requests.post(regigsterUrl, json = registeredObject)
+    print("--------------------------------------------------")
+    print("SUCESSFULLY GENERATED "+str(inputCount)+ " NUMBERS")
 
-    print ("register",rowCount, concat(0, randomNum), x.text, x)
+def main():
+    try:
+        inputCount = int(input('HOW MANY NUMBER YOU NEED TO GENERATE ? '))
+        valid = True 
+        print("GENERATING ",inputCount , " NUMBERS")
+        print('-------------------------------------')
+        generate(inputCount)
+    except ValueError:
+        print('INVALIDA AMOUNT ENTERED, TRY AGAIN')
+        print('-------------------------------------')
+        main()
 
-    loginUrl = 'https://fuelpass.gov.lk/api/otp/registration'
-    loginObject = {'mobileNo': concat(0, randomNum)}
-    y = requests.post(loginUrl, json = loginObject)
-    print ("login",rowCount, concat(0, randomNum), y.text, y)
+    
+
+if __name__ == "__main__":
+    main()
